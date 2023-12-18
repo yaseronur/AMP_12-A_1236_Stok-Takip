3112ccd07ffa9907c0d682c5c8fd180551336949
Öğrenci <renci@B4-03>
2023-12-18 10:15:51 +0300

    ghh

---
+++ WpfApp1/Modeller/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Modeller
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Müşteri> Müşteriler { get; set; }
+        public DbSet<Personel> Personeller { get; set; }
+        public DbSet<Satış> Satışlar { get; set; }
+        public DbSet<SatışDetayı> SatışDetayları { get; set; }
+        public DbSet<Tedarikçi> Tedarikçiler { get; set; }
+        public DbSet<Ürün> Ürünler { get; set; }
+        public DbSet<Yetki> Yetkiler { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=|DataDirectory|Db.mdf;")
+                .UseLazyLoadingProxies();
+        }
+    }
+   
+}
---
+++ WpfApp1/Modeller/Db.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Modeller
+{
+    public static class Db
+    {
+        public static AppDbContext Context;
+        public static void Initialize()
+        {
+            Context = new AppDbContext();
+            Context.Database.EnsureCreated();
+        }
+    }
+}
